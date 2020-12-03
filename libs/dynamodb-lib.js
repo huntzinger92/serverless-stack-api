fd8dbf20ecb6efe1a7e1cbc9e919095724778c4e
Trevor Smith <tts2p4@mail.umkc.edu>
2020-12-03 16:31:04 -0600

    Progress on serverless tutorial

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,14 @@
+import AWS from "aws-sdk";
+
+//not included in the tutorial, but necessary for you
+AWS.config.update({ region: "us-east-2" });
+
+const client = new AWS.DynamoDB.DocumentClient();
+
+export default {
+  get: (params) => client.get(params).promise(),
+  put: (params) => client.put(params).promise(),
+  query: (params) => client.query(params).promise(),
+  update: (params) => client.update(params).promise(),
+  delete: (params) => client.delete(params).promise(),
+};
